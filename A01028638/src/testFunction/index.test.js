f1233e4bf1031961b47ca4c751c48519712317e8
Pablo Rocha <pablorochatmm@gmail.com>
2023-05-30 20:29:55 -0600

    run tests in pipeline

---
+++ A01028638/src/testFunction/index.test.js
@@ -0,0 +1,17 @@
+const { handler } = require("./index.js");
+
+describe("Lambda handler", () => {
+  it("should return the correct response", async () => {
+    const mockEvent = {}; // Fill this in with a representative event object, if necessary
+    const mockContext = {}; // Fill this in with a representative context object, if necessary
+
+    const expectedResponse = {
+      statusCode: 200,
+      body: JSON.stringify("Hello from Lambda!"),
+    };
+
+    const response = await handler(mockEvent, mockContext);
+
+    expect(response).toEqual(expectedResponse);
+  });
+});
