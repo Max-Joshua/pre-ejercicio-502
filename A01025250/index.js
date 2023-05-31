3dafc63e9f95f79e964bea1ca747f37e614ea691
Andrea Yela <73191386+AnYelg@users.noreply.github.com>
2023-05-30 20:31:00 -0600

    Build

---
+++ A01025250/index.js
@@ -0,0 +1,17 @@
+exports.handler = async (event, context) => {
+  try {
+    console.log(event)
+
+    // Returning a response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello friend' }),
+    };
+  } catch (error) {
+    // Handling any errors that might occur
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'An error occurred' }),
+    };
+  }
+};
