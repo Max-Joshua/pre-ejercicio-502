bd061e335bf3845f0dbb03ccfd552cadd8f2dc48
lfvm <lfvm200@gmail.com>
2023-05-31 10:27:41 -0600

    initial commit

---
+++ A01745186/test/lambda_test.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lambda_function import handler
+
+
+def test_lambda_handler():
+    event = {}
+    context = {}
+    response = handler(event, context)
+    assert response['statusCode'] == 200
