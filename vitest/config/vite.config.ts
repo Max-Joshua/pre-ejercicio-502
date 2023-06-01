4e1bd7202eb724859fc5f98853593f9253b13d5d
A01781042 <90709790+A01781042@users.noreply.github.com>
2023-05-31 18:56:43 -0600

    test

--- vitest/config/vite.config.ts
+++ vitest/config/vite.config.ts
@@ -1,21 +1,29 @@
-import { defineConfig } from "vitest/config";
+// import { defineConfig } from "vitest/config";
 
-// vite.config.js
-export default {
-  include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-  exclude: [
-    "**/node_modules/**",
-    "**/dist/**",
-    "**/cypress/**",
-    "**/.{idea,git,cache,output,temp}/**",
-    "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
-  ],
-  optimizeDeps: {
-    exclude: ["cypress"],
+// // vite.config.js
+// export default {
+//   include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+//   exclude: [
+//     "**/node_modules/**",
+//     "**/dist/**",
+//     "**/cypress/**",
+//     "**/.{idea,git,cache,output,temp}/**",
+//     "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+//   ],
+//   optimizeDeps: {
+//     exclude: ["cypress"],
+//   },
+//   server: {
+//     watch: {
+//       exclude: ["**/node_modules/**", "**/dist/**"],
+//     },
+//   },
+// };
+
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    exclude: [...configDefaults.exclude, "packages/template/*"],
   },
-  server: {
-    watch: {
-      exclude: ["**/node_modules/**", "**/dist/**"],
-    },
-  },
-};
+});
