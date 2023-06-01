c448569c51a3beb53146962271c30944d457196b
A01781042 <90709790+A01781042@users.noreply.github.com>
2023-05-31 19:12:46 -0600

    fix

--- A01781042/vite.config.ts
+++ A01781042/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    // ...
+// vite.config.js
+export default {
+  include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  exclude: [
+    "**/node_modules/**",
+    "**/dist/**",
+    "**/cypress/**",
+    "**/.{idea,git,cache,output,temp}/**",
+    "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+  ],
+  optimizeDeps: {
+    exclude: ["cypress"],
   },
-});
+  server: {
+    watch: {
+      exclude: ["**/node_modules/**", "**/dist/**"],
+    },
+  },
+};
+
+// import { defineConfig } from "vitest/config";
+
+// export default defineConfig({
+//   test: {
+//     // ...
+//   },
+// });
