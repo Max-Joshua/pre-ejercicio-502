de8dffeee05d7cc27556c0583cdbe62c91da3034
A01781042 <90709790+A01781042@users.noreply.github.com>
2023-05-31 20:38:25 -0600

    fix

--- A01781042/jest.config.js
+++ A01781042/jest.config.js
@@ -1,5 +1,8 @@
 //Jest-config.js
 module.exports = {
-  testMatch: ["**/*.test.js"], // Specify the pattern for test files
+  testMatch: ["**/*.test.js"],
+  testEnvironment: "node",
+  verbose: true,
+  detectOpenHandles: true, // Specify the pattern for test files
   // Add any other Jest configuration options as needed
 };
