b0a9da6f89578df4ea689aefd66e4dc2cb6be8a2
Jacobo Soffer <sofferjacob@gmail.com>
2023-06-01 20:39:21 -0600

    fix tf 6

--- A01028653/role.tf
+++ A01028653/role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "a01028653_role_210421" {
-  name = "a01028653_role_210421"
+resource "aws_iam_role" "a01028653_role_21042104" {
+  name = "a01028653_role_21042104"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
