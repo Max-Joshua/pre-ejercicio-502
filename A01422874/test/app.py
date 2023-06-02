6a86ee4a917b46044ab3084425bed2f7a1ffcfa7
Salvador Salgado <chavasalgado27@gmail.com>
2023-06-01 20:43:12 -0600

    Update yml and function

--- A01422874/test/app.py
+++ A01422874/test/app.py
@@ -1,16 +1,8 @@
-# 8 Ball Magic
+import json
 
-import math
-import os
-import sys
-import random
-
-def main():
-    # Maigc 8 Puzzle
-    answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again']
-
-    num_answers = len(answers)
-    
-    print('Welcome to the Magic 8-Ball!')
-    answer = random.randint(0, num_answers-1)
-    return answers[answer]
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
